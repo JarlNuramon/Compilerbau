46fd970426120b5d6071f8a2cec4dcfaba84657d
Sadegh Zadeh Farid <saied.sadegh-zadeh-farid@adesso.de>
2022-03-11 14:36:39 +0100

    syntax Tree basics

--- src/ownCompiler/ParserApplication.java
+++ src/ownCompiler/ParserApplication.java
@@ -1,18 +1,18 @@
 package ownCompiler;
 
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
+
 import java.util.List;
 
 public class ParserApplication {
 
-	List<String> lines = new ArrayList<>();
+	
 	public static void main(String[] args) {
-		try(BufferedReader bR = Files.newBufferedReader(Paths.get("Code.sdk"))){
-		
+		try{
+			List<String> lines = Files.lines(Paths.get("Code.sdk"),StandardCharsets.UTF_8).toList();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
