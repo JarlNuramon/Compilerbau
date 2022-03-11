0e77345a7e7cb06d070402463ce34c52cef5718b
Sadegh Zadeh Farid <saied.sadegh-zadeh-farid@adesso.de>
2022-03-11 14:34:09 +0100

    syntax Tree basics

--- src/ownCompiler/ParserApplication.java
+++ src/ownCompiler/ParserApplication.java
@@ -1,8 +1,20 @@
 package ownCompiler;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParserApplication {
 
+	List<String> lines = new ArrayList<>();
 	public static void main(String[] args) {
+		try(BufferedReader bR = Files.newBufferedReader(Paths.get("Code.sdk"))){
 		
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- src/ownCompiler/SyntaxTree.java
+++ src/ownCompiler/SyntaxTree.java
@@ -5,7 +5,39 @@ import java.util.List;
 
 public class SyntaxTree {
 
-	List<SyntaxTree> children = new LinkedList<>();
-	String value;
-	Function function;
+	private List<SyntaxTree> children ;
+	private String value;
+	private Function function;
+
+	
+	public SyntaxTree(Function function) {
+		this.function = function;
+		this.value=null;
+		this.children = new LinkedList<>();
+	}
+	public SyntaxTree(Function function, String value) {
+		this.function = function;
+		this.value=value;
+		this.children = new LinkedList<>();
+	}
+	public List<SyntaxTree> getChildren() {
+		return children;
+	}
+	public void setChildren(List<SyntaxTree> children) {
+		this.children = children;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	public Function getFunction() {
+		return function;
+	}
+	public void setFunction(Function function) {
+		this.function = function;
+	}
+	
+	
 }
